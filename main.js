7aabc523b4b216ced8feadf97659f10356deae74
Amayas-Tamarat <amayas.tamarat@gmail.com>
2023-06-19 16:19:04 +0200

    end exo 3

---
+++ main.js
@@ -0,0 +1,28 @@
+
+/*
+EXO 1
+let a = 45
+
+alert(a)
+*/
+
+
+
+
+// let prenom = "Amayas"
+// let nom = "Tamarat"
+// let city = "Sainté"
+
+// alert("prenom : " + prenom + "\n" + "nom : " + nom + "\n" + "city : " + city )
+
+
+
+/*
+EXO 3
+let prenom = prompt("entrez prenom : ");
+alert("Bonjour, " + prenom)
+*/
+
+
+
+
